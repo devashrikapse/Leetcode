8dd188f97ffd5fa11d5bcd0a9e9357db491b63bb
devashrikapse <devashrikapse@gmail.com>
2026-08-19 23:21:43 +0530

    19-08-Fast-Slow pointer

---
+++ Slow-Fast pointer/03_19. Remove Nth Node From End of List.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        slow = dummy
+        fast = dummy
+
+        # Move fast n + 1 steps
+        for _ in range(n + 1):
+            fast = fast.next
+
+        # Move both pointers
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+
+        # Remove the nth node
+        slow.next = slow.next.next
+
+        return dummy.next
